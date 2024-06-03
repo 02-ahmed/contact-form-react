f27399eefcb7aefd06608e3549ce629deb71f59b
02-ahmed <ahmedboabae2@gmail.com>
2024-06-03 12:04:46 +0000

    updated the contact form to use react hook form

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,19 @@
 import { useState } from "react";
 import "./App.css";
 import ContactForm from "./components/ContactForm";
-import Button from "./components/Button";
+
 
 
 function App() {
-  const [canSubmit, setCanSubmit] = useState(false);
+  
   const [isChecked, setIsChecked] = useState(false);
   const [isChecked2, setIsChecked2] = useState(false);
-
-  const [firstNameValid, setFirstNameValid] = useState(true);
-  const [secondNameValid, setSecondNameValid] = useState(true);
-  const [emailValid, setemailValid] = useState(true);
-
-  const [queryValid, setQueryValid] = useState(true);
-
-  const [messageValid, setMessageValid] = useState(true);
-  const [consentValid, setConsentValid] = useState(true);
-
-  const handleFirstName = (event:React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value.length> 0){
-      setFirstNameValid(false)
-    }
-  }
-
-  const handleSecondName = (event:React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value.length> 0){
-      setSecondNameValid(false)
-    }
-  }
-
-  const handleEmail = (event:React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.value.length> 0){
-      setemailValid(false)
-    }
-  }
-
-  const handleMessage = (event:React.ChangeEvent<HTMLTextAreaElement>) => {
-    if (event.target.value.length> 0){
-      setMessageValid(false)
-    }
-  }
-
-  const handleConsent = (event:React.ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked === true){
-      setConsentValid(false)
-    }else{
-      setConsentValid(true)
-    }
-  }
-
-
-  const handleClick = () => {
-
-    setCanSubmit(!canSubmit)
-  }
 
   const handleDivClick = () => {
     if (isChecked === false ){
       setIsChecked2(false)
       setIsChecked(true)
-      setQueryValid(false)
+      
       
     }else{
       setIsChecked(false)
@@ -70,40 +23,16 @@ function App() {
     if (isChecked2 === false){
       setIsChecked(false)
       setIsChecked2(true)
-      setQueryValid(false)
+      
     }else{
       setIsChecked2(false)
     }
   }
 
-  const handleQuery = () => {
-    if (isChecked === false || isChecked2 === false){
-      setQueryValid(false)
-    }
-  }
-
+  
   return(
     <>
-      <ContactForm className={`invalid-feedback ${canSubmit ? 'show':'' }`} divClick={handleDivClick} divClick2={handleDivClick2} isChecked={isChecked} isChecked2={isChecked2}
-      firstNameClass={`invalid-feedback ${firstNameValid ? 'show':'' }`}
-      handleFirstName={handleFirstName}
-
-      secondNameClass={`invalid-feedback ${secondNameValid ? 'show':'' }`}
-      handleSecondName={handleSecondName}
-
-      emailClass={`invalid-feedback ${emailValid ? 'show':'' }`}
-      handleEmail={handleEmail}
-
-      queryClass={`invalid-feedback ${queryValid ? 'show':'' }`}
-      handleQuery = {handleQuery}
-
-      messageClass={`invalid-feedback ${messageValid ? 'show':'' }`}
-      handleMessage = {handleMessage}
-
-      consentClass={`invalid-feedback ${consentValid ? 'show':'' }`}
-      handleConsent = {handleConsent}
-      >
-        <Button onClick={handleClick} ></Button>
+      <ContactForm divClick={handleDivClick} divClick2={handleDivClick2} isChecked={isChecked} isChecked2={isChecked2}>  
       </ContactForm>
     </>
   )
--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,88 +1,90 @@
-import { ReactNode } from "react";
+
 import "../App.css";
+import {FieldValues, useForm} from 'react-hook-form';
 
 interface Props {
- children: ReactNode;
- className: string;
  divClick : () => void;
  divClick2: () => void;
  isChecked: boolean;
  isChecked2: boolean;
- firstNameClass: string;
- secondNameClass: string;
- emailClass: string;
- queryClass: string;
- messageClass: string;
- consentClass: string;
- handleFirstName: (event:React.ChangeEvent<HTMLInputElement>) => void
- handleSecondName: (event:React.ChangeEvent<HTMLInputElement>) => void
- handleEmail: (event:React.ChangeEvent<HTMLInputElement>) => void
- handleQuery: () => void
- handleMessage: (event:React.ChangeEvent<HTMLTextAreaElement>) => void
- handleConsent: (event:React.ChangeEvent<HTMLInputElement>) => void
+ 
 }
 
-const ContactForm = ({children, divClick, divClick2, isChecked, isChecked2, firstNameClass, secondNameClass, emailClass,queryClass,messageClass,consentClass, handleFirstName, handleSecondName,handleEmail, handleMessage, handleConsent}:Props) => {
+interface FormData {
+  firstName: string,
+  lastName: string,
+  email: string,
+  query: boolean,
+  message: string,
+  check: boolean
+}
+
+const ContactForm = ({divClick, divClick2}:Props) => {
+
+  const {register, handleSubmit, formState:{errors}} = useForm<FormData>();
+
+  const onSubmit = (data: FieldValues) => {
+    console.log(data)
+  }
+
+
   return (
     <>
       <div className="container">
         <h2>Contact Us</h2>
-        <form className="row g-3 needs-validation" noValidate>
+        <form className="row g-3 needs-validation" onSubmit={handleSubmit(onSubmit)}>
           <div className="col-md-6">
-            <label htmlFor="validationCustom01" className="form-label" >First name</label>
-            <input type="text" className="form-control" id="validationCustom01" onChange={handleFirstName} required/>
-            <div className={firstNameClass}>
-              This field is required.
-            </div>
+            <label htmlFor="firstName" className="form-label" >First name</label>
+            <input type="text" className="form-control" id="firstName"  {...register('firstName', {required:true})}/>
+            {errors.firstName?.type==="required" && <p className="text-danger">The name field is required</p>}
           </div>
           <div className="col-md-6">
-            <label htmlFor="validationCustom02" className="form-label">Last name</label>
-            <input type="text" className="form-control" id="validationCustom02" onChange={handleSecondName} required/>
-            <div className={secondNameClass}>
-              This field is required.
-            </div>
+            <label htmlFor="lastName" className="form-label">Last name</label>
+            <input type="text" className="form-control" id="lastName" {...register('lastName', {required:true})}/>
+            {errors.lastName?.type==="required" && <p className="text-danger">This field is requried</p>}
           </div>
           <div className="col-md-12">
-            <label htmlFor="validationCustom03" className="form-label">Email Address</label>
-            <input type="text" className="form-control" id="validationCustom03" onChange={handleEmail} required />
-            <div className={emailClass}>
-              Please provide a valid email.
-            </div>
+            <label htmlFor="email" className="form-label">Email Address</label>
+            <input type="text" className="form-control" id="email" {...register('email',  {required:true, minLength:5})}/>
+            {errors.email?.type === "required" && <p className="text-danger">This field is requried</p>}
+            {errors.email?.type === "minLength" && <p className="text-danger">Enter a valid email</p>}
           </div>
 
           <label htmlFor="queryValidation" className="form-label query">Query Type</label>
-          <div className="col-md-6 query-type left-query" onClick={divClick2}>
-            <input type="radio" name="query" checked={isChecked2} />General Enquiry
+          <div className="col-md-6 query-type left-query">
+            <label htmlFor="queryValidation1">
+              <div /* className="col-md-6 query-type left-query" */  onClick={divClick2}>
+                <input type="radio" id="queryValidation1" {...register('query', {required:true})}/>General Enquiry
+              </div>
+            </label>
+            <p className="text-danger"></p>
           </div>
-          <div className="col-md-6  query-type right-query" onClick={divClick}>
-            <input type="radio" name="query" id="" checked={isChecked}/>Support Request
+          <div className="col-md-6 query-type right-query">
+            <label htmlFor="queryValidation2">
+              <div /* className="col-md-6  query-type right-query" */ onClick={divClick}>
+                <input type="radio"  id="queryValidation2" {...register('query', {required:true})}/>Support Request
+              </div>
+            </label>
           </div>
-          <div className={queryClass}>
-              Please select a query type.
-          </div>
+          {errors.query?.type === 'required' && <p className="text-danger">select one</p>}
 
           <div className="col-12">
             <label htmlFor="message" className="col-12 ">Message</label>
-            <textarea className= "col-12 textarea form-control" name="" id="" onChange={handleMessage}></textarea>
-            <div className={messageClass}>
-              This field is required
-            </div>
-
+            <textarea className= "col-12 textarea form-control" id="message" {...register('message', {required:true})}></textarea>
+            {errors.message?.type === "required" && <p className="text-danger">This field is required</p>}
           </div>
 
 
           <div className="col-12">
             <div className="form-check">
-              <input className="form-check-input" type="checkbox" value="" id="invalidCheck" onChange={handleConsent} required />
-              <label className="form-check-label" htmlFor="invalidCheck">
+              <input className="form-check-input" type="checkbox" id="check" {...register('check', {required:true})} />
+              <label className="form-check-label" htmlFor="check">
                 I consent to being contacted by the team
               </label>
-              <div className={consentClass}>
-                To submit this form, please consent to being contacted.
-              </div>
             </div>
+            {errors.check?.type === 'required' && <p className="text-danger">You must consent</p>}
           </div> 
-          {children}
+          <button className="btn btn-success col-12" type="submit">Submit</button>
         </form>
       </div>
       
